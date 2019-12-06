e1472b94159cadb95bc2e85e0bbfddfa7779587a
anyapress777@gmail.com <golovinzodchiy95>
2019-12-06 11:35:32 +0200

    task 4.3 was finished

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        String essentialText = text.replaceAll("[^a-zA-Z]", "");
+        String reversedEssentialText = new StringBuilder(essentialText).reverse().toString();
+        return essentialText.equalsIgnoreCase(reversedEssentialText);
     }
 }
